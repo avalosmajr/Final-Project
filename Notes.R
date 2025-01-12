18b081638530c8ada6e13736db30ed2542af340e
Michael Avalos Jr <michaelavalosjr.@MacBook-Pro-3.local>
2025-01-12 01:15:44 -0600

    Notes from readings

---
+++ Notes.R
@@ -0,0 +1,3 @@
+2+2
+Writing your code in a script means that you’ll always have a permanent record of everything you’ve done (provided you save your script) and also allows you to make loads of comments to remind your future self what you’ve done.
+try and see the diff between # and keeping notes in r script
